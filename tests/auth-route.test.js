fd745431eb56b9c77655178356faf6a3c639983d
Charlely Delgrange <charlely.delgrange@gmail.com>
2022-03-29 16:29:54 +0200

    add some tests

--- tests/auth-route.test.js
+++ tests/auth-route.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 
-test('POST /login', async () => {
+test('POST /login => success', async () => {
   const res = await request(apiUrl)
     .post('/login')
     .send({
@@ -8,6 +8,41 @@ test('POST /login', async () => {
       password: 'password',
     });
 
-  expect(res.statusCode).toEqual(200)
+  expect(res.statusCode).toEqual(200);
   expect(res.body).toHaveProperty('token');
+});
+
+test('POST /login => without firstName fail', async () => {
+  const res = await request(apiUrl)
+    .post('/login')
+    .send({
+      password: 'password',
+    });
+
+  expect(res.statusCode).toEqual(500);
+  expect(res.text).toEqual('Property "req.body.firstName": Invalid value. Current value = undefined');
+});
+
+test('POST /login => with wrong firstName fail', async () => {
+  const res = await request(apiUrl)
+    .post('/login')
+    .send({
+      firstName: 'fake',
+      password: 'password',
+    });
+
+  expect(res.statusCode).toEqual(401);
+  expect(res.text).toEqual('Unauthorized');
+});
+
+test('POST /login => with wrong password fail', async () => {
+  const res = await request(apiUrl)
+    .post('/login')
+    .send({
+      firstName: 'Charlely',
+      password: 'fake',
+    });
+
+  expect(res.statusCode).toEqual(401);
+  expect(res.text).toEqual('Unauthorized');
 });
---
+++ tests/user-create.test.js
@@ -0,0 +1,64 @@
+const request = require('supertest');
+
+describe('Test without jwt', () => {
+  test('POST /users without jwt', async () => {
+    const res = await request(apiUrl).post('/users');
+
+    expect(res.statusCode).toEqual(500);
+  });
+})
+
+describe('Test with jwt', () => {
+  let jwt = '';
+
+  beforeAll(async () => {
+    const res = await request(apiUrl)
+      .post('/login')
+      .send({
+        firstName: 'Charlely',
+        password: 'password',
+      });
+
+      jwt = res.body.token;
+  });
+
+  test('POST /users => success', async () => {
+    const res = await request(apiUrl)
+      .post('/users')
+      .set('Authorization', `Bearer ${jwt}`)
+      .send({
+        firstName: 'Blablabla',
+        lastName: 'Blublublu',
+        password: 'Blobloblo',
+      });
+
+    expect(res.statusCode).toEqual(201);
+  });
+
+  test('POST /users => without "lastName" should fail', async () => {
+    const res = await request(apiUrl)
+      .post('/users')
+      .set('Authorization', `Bearer ${jwt}`)
+      .send({
+        firstName: 'Blablabla',
+        password: 'Blobloblo',
+      });
+
+    expect(res.statusCode).toEqual(500);
+    expect(res.text).toEqual('Property "req.body.lastName": Invalid value. Current value = undefined');
+  });
+
+  test('POST /users', async () => {
+    const res = await request(apiUrl)
+      .post('/users')
+      .set('Authorization', `Bearer ${jwt}`)
+      .send({
+        firstName: 'Blablabla',
+        lastName: 'Blublublu',
+        password: 'oups',
+      });
+
+    expect(res.statusCode).toEqual(500);
+    expect(res.text).toEqual('Property "req.body.password": Invalid value. Current value = oups');
+  });
+});
